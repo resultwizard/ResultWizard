9e955d2d9b81dfd2dd2a98ed8db5a4a4e45589f4
Splines <dominic-plein@gmx.de>
2024-02-17 10:57:15 +0100

    Fix too long line

--- tests/playground.py
+++ tests/playground.py
@@ -1,8 +1,10 @@
 # TLDR: `pip install -e .` from the root directory of the package
 # Then: `python3 ./tests/playground.py`
 #
-# development mode: "setuptools allows you to install a package without copying any files to your interpreter directory (e.g. the site-packages directory)." from [1]
-# [1] https://setuptools.pypa.io/en/latest/userguide/quickstart.html#development-mode
+# development mode: "setuptools allows you to install a package without
+# copying any files to your interpreter directory
+# (e.g. the site-packages directory)."
+# From: https://setuptools.pypa.io/en/latest/userguide/quickstart.html#development-mode
 
 
 from valuewizard import res
