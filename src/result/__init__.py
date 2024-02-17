ab59f3a7e9bdff23ad6f6285304f2127e729ca17
Splines <dominic-plein@gmx.de>
2024-02-17 01:23:45 +0100

    Init `valuewizard.result` module

---
+++ src/result/__init__.py
@@ -0,0 +1,3 @@
+def res(number):
+    print("hi there from res")
+    print(f"number is {number}")
---
+++ tests/playground.py
@@ -0,0 +1,11 @@
+# TLDR: `pip install -e .` from the root directory of the package
+# Then: `python3 ./tests/playground.py`
+#
+# development mode: "setuptools allows you to install a package without copying any files to your interpreter directory (e.g. the site-packages directory)." from [1]
+# [1] https://setuptools.pypa.io/en/latest/userguide/quickstart.html#development-mode
+
+
+import valuewizard.result as wiz
+
+
+wiz.res(1.0)
