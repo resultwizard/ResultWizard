0ba323160be4a4d156aaaa1b67f03f886075b32e
paul019 <39464035+paul019@users.noreply.github.com>
2024-03-14 12:14:03 +0100

    Update __init__.py

--- src/valuewizard/__init__.py
+++ src/valuewizard/__init__.py
@@ -1,4 +1,7 @@
 from api.res import res
+from api.tables.table import table
+from api.tables.column import column
+from api.tables.table_res import table_res
 from api.export import export
 from application.cache import _ResultsCache
 
