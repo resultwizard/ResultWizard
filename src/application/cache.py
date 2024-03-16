7fb8e1ad7be30948d248ac5ee4e232b030c9e96f
paul019 <39464035+paul019@users.noreply.github.com>
2024-03-16 14:21:17 +0100

    Add a warning when shadowing values in cache.

--- src/application/cache.py
+++ src/application/cache.py
@@ -6,13 +6,17 @@ class ResultsCache:
     A cache for all user-defined results. Results are hashed by their name.
     If the user tries to add a result with a name that already exists in the cache,
     the new result will replace the old one ("shadowing").
-    # TODO: is this the wanted behavior? Maybe print a warning in this case.
     """
 
     def __init__(self):
         self.cache: dict[str, Result] = {}
 
     def add(self, name, result: Result):
+        if name in self.cache:
+            print(
+                f"Warning: A result with the name '{name}' already exists and will be overwritten."
+            )
+
         self.cache[name] = result
 
     def get_all_results(self) -> list[Result]:
