398df1c33c2b3d16c0b1714e2d82ee481d223c3a
paul019 <39464035+paul019@users.noreply.github.com>
2024-03-19 12:39:15 +0100

    Restructure rounder test and test all rounding hierarchies

--- tests/rounder_test.py
+++ tests/rounder_test.py
@@ -1,5 +1,6 @@
 # pylint: disable=redefined-outer-name
 
+from typing import List
 from decimal import Decimal
 import pytest
 
@@ -15,115 +16,206 @@ def config_defaults():
 
 
 class TestRounder:
-    def test_hierarchy_1(self, config_defaults):
-        res = Result("", Value(Decimal("1.0000"), min_exponent=-4), "", [], 2, 10)
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -4
 
-        res = Result(
-            "",
-            Value(Decimal("1.0000"), min_exponent=-4),
-            "",
-            [Uncertainty(Value(Decimal("0.1")))],
-            None,
-            None,
-        )
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -4
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -4
-
-        res = Result(
-            "",
-            Value(Decimal("1.0000"), min_exponent=-4),
-            "",
-            [Uncertainty(Value(Decimal("0.1"), min_exponent=-1))],
-            None,
-            None,
-        )
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -4
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -1
-
-    def test_hierarchy_3(self, config_defaults):
-        res = Result("", Value(Decimal("1.0")), "", [], 10, 2)
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -9
-
-        res = Result("", Value(Decimal("1.0")), "", [Uncertainty(Value(Decimal("0.1")))], 10, None)
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -9
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -9
-
-        res = Result(
-            "",
-            Value(Decimal("1.0")),
-            "",
-            [Uncertainty(Value(Decimal("0.1"), min_exponent=-1))],
-            10,
-            None,
-        )
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -9
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -1
-
-    def test_hierarchy_4(self, config_defaults):
-        res = Result("", Value(Decimal("1.0")), "", [], None, 2)
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -2
-
-        res = Result("", Value(Decimal("1.0")), "", [Uncertainty(Value(Decimal("0.1")))], None, 2)
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -2
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -2
-
-        res = Result(
-            "",
-            Value(Decimal("1.0")),
-            "",
-            [Uncertainty(Value(Decimal("0.1e-5"), min_exponent=-6))],
-            None,
-            2,
-        )
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -2
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -6
-
-    def test_hierarchy_5(self, config_defaults):
-        res = Result(
-            "", Value(Decimal("1.0")), "", [Uncertainty(Value(Decimal("0.11")))], None, None
-        )
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -2
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -2
-
-        res = Result(
-            "", Value(Decimal("1.0")), "", [Uncertainty(Value(Decimal("0.294999")))], None, None
-        )
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -2
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -2
-
-        res = Result(
-            "", Value(Decimal("1.0")), "", [Uncertainty(Value(Decimal("0.295001")))], None, None
-        )
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -1
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -1
-
-        res = Result(
-            "",
-            Value(Decimal("1.0")),
-            "",
-            [Uncertainty(Value(Decimal("0.4"))), Uncertainty(Value(Decimal("0.04")))],
-            None,
-            None,
-        )
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -2
-        assert res.uncertainties[0].uncertainty.get_min_exponent() == -1
-        assert res.uncertainties[1].uncertainty.get_min_exponent() == -2
-
-    def test_hierarchy_6(self, config_defaults):
-        res = Result("", Value(Decimal("1.0")), "", [], None, None)
-        Rounder.round_result(res, config_defaults)
-        assert res.value.get_min_exponent() == -1
+    @pytest.mark.parametrize(
+        "result, config, expected_value_min_exponent, expected_uncert_min_exponents",
+        [
+            # Hierarchy 1:
+            (
+                Result("", Value(Decimal(1.0), -1), "", [], None, None),
+                RoundingConfig(-1, -1, 2, -1),
+                -1,
+                [],
+            ),
+            (
+                Result(
+                    "", Value(Decimal(1.0), -1), "", [Uncertainty(Value(Decimal(1.0)))], None, None
+                ),
+                RoundingConfig(-1, -1, 2, -1),
+                -1,
+                [-1],
+            ),
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0), -1),
+                    "",
+                    [Uncertainty(Value(Decimal(1.0), -2))],
+                    None,
+                    None,
+                ),
+                RoundingConfig(-1, -1, 2, -1),
+                -1,
+                [-2],
+            ),
+            # Hierarchy 2:
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [Uncertainty(Value(Decimal(1.0)))],
+                    3,
+                    None,
+                ),
+                RoundingConfig(-1, -1, 2, -1),
+                -2,
+                [-2],
+            ),
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [Uncertainty(Value(Decimal(1.0), -3))],
+                    3,
+                    None,
+                ),
+                RoundingConfig(-1, -1, 2, -1),
+                -2,
+                [-3],
+            ),
+            # Hierarchy 3:
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [Uncertainty(Value(Decimal(1.0)))],
+                    None,
+                    2,
+                ),
+                RoundingConfig(-1, -1, 2, -1),
+                -2,
+                [-2],
+            ),
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [Uncertainty(Value(Decimal(1.0), -3))],
+                    None,
+                    2,
+                ),
+                RoundingConfig(-1, -1, 2, -1),
+                -2,
+                [-3],
+            ),
+            # Hierarchy 4:
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [Uncertainty(Value(Decimal(1.0)))],
+                    None,
+                    None,
+                ),
+                RoundingConfig(5, -1, 2, -1),
+                -4,
+                [-4],
+            ),
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [Uncertainty(Value(Decimal(1.0), -3))],
+                    None,
+                    None,
+                ),
+                RoundingConfig(5, -1, 2, -1),
+                -4,
+                [-3],
+            ),
+            # Hierarchy 5:
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [Uncertainty(Value(Decimal(1.0)))],
+                    None,
+                    None,
+                ),
+                RoundingConfig(-1, 4, 2, -1),
+                -4,
+                [-4],
+            ),
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [Uncertainty(Value(Decimal(1.0), -3))],
+                    None,
+                    None,
+                ),
+                RoundingConfig(-1, 4, 2, -1),
+                -4,
+                [-3],
+            ),
+            # Hierarchy 6:
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [
+                        Uncertainty(Value(Decimal(1.0), -3)),
+                        Uncertainty(Value(Decimal(1.0))),
+                        Uncertainty(Value(Decimal(2.0))),
+                        Uncertainty(Value(Decimal(2.9499))),
+                        Uncertainty(Value(Decimal(2.9500))),
+                        Uncertainty(Value(Decimal(0.007))),
+                    ],
+                    None,
+                    None,
+                ),
+                RoundingConfig(-1, -1, 2, -1),
+                -3,
+                [-3, -1, -1, -1, 0, -3],
+            ),
+            # Hierarchy 7:
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [],
+                    None,
+                    None,
+                ),
+                RoundingConfig(-1, -1, 2, -1),
+                -1,
+                [],
+            ),
+            # Hierarchy 8:
+            (
+                Result(
+                    "",
+                    Value(Decimal(1.0)),
+                    "",
+                    [],
+                    None,
+                    None,
+                ),
+                RoundingConfig(-1, -1, -1, 2),
+                -2,
+                [],
+            ),
+        ],
+    )
+    def test_all_hierarchies(
+        self,
+        result: Result,
+        config: RoundingConfig,
+        expected_value_min_exponent: int,
+        expected_uncert_min_exponents: List[int],
+    ):
+        Rounder.round_result(result, config)
+        assert result.value.get_min_exponent() == expected_value_min_exponent
+        assert [
+            u.uncertainty.get_min_exponent() for u in result.uncertainties
+        ] == expected_uncert_min_exponents
